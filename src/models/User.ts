074b118b297b95699509bd6e0f48406c7e9ef3a4
Hasmukhsingh1999 <hasmukhsingh90@gmail.com>
2024-06-19 12:31:50 +0530

    Project Setup

---
+++ src/models/User.ts
@@ -0,0 +1,85 @@
+import mongoose, { Schema, Document, Mongoose } from "mongoose";
+//Document - type safety
+
+// Document type safety interfaces
+export interface IMessage extends Document {
+  content: string;
+  createdAt: Date;
+}
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerified: boolean;
+  isAcceptingMessage: boolean;
+  messages: IMessage[];
+}
+
+// Message schema
+const MessageSchema: Schema<IMessage> = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+// User schema
+const UserSchema: Schema<IUser> = new Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required."],
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required."],
+      unique: true,
+      match: [/.+\@.+\..+/, "Please use a valid email address"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    verifyCode: {
+      type: String,
+      required: [true, "Verify code is required"],
+    },
+    verifyCodeExpiry: {
+      type: Date,
+      required: [true, "Verify Code expiry is required"],
+      default: () => new Date(Date.now() + 24 * 60 * 60 * 1000), // Default to 24 hours from now
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isAcceptingMessage: {
+      type: Boolean,
+      default: true,
+    },
+    messages: [MessageSchema],
+  },
+  { timestamps: true }
+);
+
+const UserModel =
+  (mongoose.models.User as mongoose.Model<IUser>) ||
+  mongoose.model<IUser>("User", UserSchema);
+
+const MessageModel =
+  (mongoose.models.Message as mongoose.Model<IMessage>) ||
+  mongoose.model<IMessage>("Message", MessageSchema);
+
+export default UserModel;
